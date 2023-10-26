1f94defb9dc05d91eda94572aa5e3f934f559595
aminhnh <109867158+aminhnh@users.noreply.github.com>
2023-10-26 13:57:11 +0700

    feat: install breeze starter kit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BukuController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +16,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('buku');
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
 // Route::resource('/buku', BukuController::class);
 Route::get('/buku', [BukuController::class, 'index'])->name('buku');
@@ -29,4 +43,3 @@ Route::post('/buku/edit/{id}', [BukuController::class, 'edit'])->name('buku.edit
 Route::post('/buku/update/{id}', [BukuController::class, 'update'])->name('buku.update');
 
 Route::get('/buku/search', [BukuController::class,'search'])->name('buku.search');
-
